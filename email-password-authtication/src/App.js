03a8fba27d4ec4aedfecaef81fdeeaede3a32fcc
anamerasul <anamerasul@gmail.com>
2022-04-12 02:31:58 +0600

    project update

--- email-password-authtication/src/App.js
+++ email-password-authtication/src/App.js
@@ -1,41 +1,48 @@
-
-import './App.css';
-
 import app from './firebase.init';
 import { getAuth } from 'firebase/auth'
 const auth = getAuth(app);
+
 function App() {
+
+  const handleEmailBlur = (event) => {
+    console.log(event.target.value);
+
+  }
+
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value);
+
+  }
+
+  const handleFormSubmit = (event) => {
+    console.log(event);
+    event.preventDefault();
+  }
   return (
-    <div className=" App ">
-      <form className="bg-white shadow-md rounded px-8 pt-16 pb-8 mb-4 max-w-md mx-auto sm:max-w-xl" style={{ 'margin': '20px' }}>
-        <div className="m-4 pt-18 style={{ 'margin': '20px' }}">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2 ml-8" style={{ 'margin': '20px' }}
-            htmlFor="username"
-          >
-            Username
-          </label>
-          <input
-            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 mx-16 block w-full appearance-none leading-normal"
-            type="email"
-            id="username"
-            placeholder="jane@example.com"
-          />
-        </div>
-        <div className="m-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2" style={{ 'margin': '20px' }}
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-            id="password"
-            type="password"
-          />
-        </div>
-      </form>
+    <div className="flex justify-center">
+      <div className="w-full max-w-xs">
+        <form onSubmit={handleFormSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 my-32">
+          <div className="mb-4">
+            <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input onBlur={handleEmailBlur} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="email" />
+          </div>
+          <div className="mb-6">
+            <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input onBlur={handlePasswordBlur} className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+            <p className="text-red text-xs italic">Please choose a password.</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <input type='submit' className="bg-blue-200 hover:bg-gray-600 hover:text-white text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" value='Sign in' />
+            <a className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="/">
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
