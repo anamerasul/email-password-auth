f831ddefa2ceb1213f255b8a891bed1b12e667f1
anamerasul <anamerasul@gmail.com>
2022-04-12 00:20:54 +0600

    react router add

--- email-password-authtication/src/index.js
+++ email-password-authtication/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 
 );
 
