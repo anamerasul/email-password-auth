6afb69b77c8bb3100e663758b25c15d9203d5225
anamerasul <anamerasul@gmail.com>
2022-04-12 04:42:34 +0600

    Login form field validation using regular expression, show error

--- email-password-authtication/src/App.js
+++ email-password-authtication/src/App.js
@@ -9,6 +9,11 @@ function App() {
 
   const [password, setPassword] = useState('')
 
+  const [error, setError] = useState('password Should contain a Capital')
+
+  const [validated, setValidated] = useState('false');
+
+  console.log(typeof validated);
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
 
@@ -20,6 +25,20 @@ function App() {
   }
 
   const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    console.log(form.checkValidity())
+    if (form.checkValidity() === false) {
+
+      event.stopPropagation();
+      return
+    }
+
+    if (!/^(?=.*[A-Z])/.test(password)) {
+      setError(' ')
+    }
+
+    setValidated('true');
 
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
@@ -30,26 +49,45 @@ function App() {
 
         console.error(error)
       })
+
+
     console.log(event, email, password);
     event.preventDefault();
+
+    console.log(validated)
+
+    return form
   }
+
+  console.log(error)
   return (
     <div className="flex justify-center">
       <div className="w-full max-w-xs">
 
-        <form onSubmit={handleFormSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 my-32">
+        <form noValidate validated={validated} onSubmit={handleFormSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 my-32">
           <h2 className="text-lg text-center bg-orange-400">Please Register</h2>
           <div className="mb-4">
             <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="Email">
               Email
             </label>
-            <input onBlur={handleEmailBlur} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="email" />
+
+            <input onBlur={handleEmailBlur} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="email" required />
+            {
+              !email ? <p className="text-red-500 text-xs italic">Please fill out this email field.</p> : ''
+            }
           </div>
+
+
+
           <div className="mb-6">
             <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
               Password
             </label>
-            <input onBlur={handlePasswordBlur} className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+            <input onBlur={handlePasswordBlur} className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" required />
+
+            {
+              !password ? <p className="text-red-500 text-xs italic">{error}.</p> : ''
+            }
             <p className="text-red text-xs italic">Please choose a password.</p>
           </div>
           <div className="flex items-center justify-between">
